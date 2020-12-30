6fd2b3e13b73ecdff43dc2cfd1666b77f8c23288
Philippe Zentner <zesi@brudi.com>
2020-12-30 17:05:28 +0100

    feat(tags): token type constants update for django 2.1

--- templatetag_handlebars/templatetags/templatetag_handlebars.py
+++ templatetag_handlebars/templatetags/templatetag_handlebars.py
@@ -1,30 +1,35 @@
 from django import template
 from django.conf import settings
-from django.utils import six
+from django.utils import safestring, six
+
+# Token type constants moved in Django 2.1
+try:
+    from django.template.base import TokenType
+except ImportError:
+
+    class TokenType(object):
+        TEXT = template.base.TOKEN_TEXT
+        VAR = template.base.TOKEN_VAR
+        BLOCK = template.base.TOKEN_BLOCK
+        COMMENT = template.base.TOKEN_COMMENT
+
 
 register = template.Library()
 
 """
-
     Most of this code was written by Miguel Araujo
     https://gist.github.com/893408
-
 """
 
 
 def verbatim_tags(parser, token, endtagname):
     """
     Javascript templates (jquery, handlebars.js, mustache.js) use constructs like:
-
     ::
-
         {{if condition}} print something{{/if}}
-
     This, of course, completely screws up Django templates,
     because Django thinks {{ and }} means something.
-
     The following code preserves {{ }} tokens.
-
     This version of verbatim template tag allows you to use tags
     like url {% url name %}. {% trans "foo" %} or {% csrf_token %} within.
     """
@@ -34,14 +39,14 @@ def verbatim_tags(parser, token, endtagname):
         if token.contents == endtagname:
             break
 
-        if token.token_type == template.base.TOKEN_VAR:
+        if token.token_type == TokenType.VAR:
             text_and_nodes.append('{{')
             text_and_nodes.append(token.contents)
 
-        elif token.token_type == template.base.TOKEN_TEXT:
+        elif token.token_type == TokenType.TEXT:
             text_and_nodes.append(token.contents)
 
-        elif token.token_type == template.base.TOKEN_BLOCK:
+        elif token.token_type == TokenType.BLOCK:
             try:
                 command = token.contents.split()[0]
             except IndexError:
@@ -58,7 +63,7 @@ def verbatim_tags(parser, token, endtagname):
                     raise
             text_and_nodes.append(node)
 
-        if token.token_type == template.base.TOKEN_VAR:
+        if token.token_type == TokenType.VAR:
             text_and_nodes.append('}}')
 
     return text_and_nodes
@@ -69,9 +74,7 @@ class VerbatimNode(template.Node):
     Wrap {% verbatim %} and {% endverbatim %} around a
     block of javascript template and this will try its best
     to output the contents with no changes.
-
     ::
-
         {% verbatim %}
             {% trans "Your name is" %} {{first}} {{last}}
         {% endverbatim %}
@@ -98,22 +101,20 @@ def verbatim(parser, token):
 
 @register.simple_tag
 def handlebars_js():
-    return """<script src="%shandlebars.js"></script>""" % settings.STATIC_URL
+    out = """<script src="%shandlebars.js"></script>""" % settings.STATIC_URL
+    return safestring.mark_safe(out)
 
 
 class HandlebarsNode(VerbatimNode):
     """
     A Handlebars.js block is a *verbatim* block wrapped inside a
     named (``template_id``) <script> tag.
-
     ::
-
         {% tplhandlebars "tpl-popup" %}
             {{#ranges}}
                 <li>{{min}} < {{max}}</li>
             {{/ranges}}
         {% endtplhandlebars %}
-
     """
     def __init__(self, template_id, text_and_nodes):
         super(HandlebarsNode, self).__init__(text_and_nodes)
@@ -133,12 +134,15 @@ class HandlebarsNode(VerbatimNode):
         </script>""" % (head_script, output)
 
 
+def stripquote(s):
+    return s[1:-1] if s[:1] == '"' else s
+
+
 @register.tag
 def tplhandlebars(parser, token):
     text_and_nodes = verbatim_tags(parser, token, endtagname='endtplhandlebars')
     # Extract template id from token
     tokens = token.split_contents()
-    stripquote = lambda s: s[1:-1] if s[:1] == '"' else s
     try:
         tag_name, template_id = map(stripquote, tokens[:2])
     except ValueError:
